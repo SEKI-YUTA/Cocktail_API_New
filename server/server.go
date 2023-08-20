afbbda336e095b3ee21074035ebbec803b015f08
SEKI-YUTA <zard.since1991@gmail.com>
2023-08-20 10:27:20 +0900

    作れるカクテルを返すエンドポイントを実装

--- server/server.go
+++ server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"cocktail_api/common"
-	"cocktail_api/setup"
 	"context"
 	"fmt"
 	"os"
@@ -69,12 +68,98 @@ func getAllIngredients() []common.Ingredient {
 	return ingredients
 }
 
+func queryCocktail(cocktailName string) common.Cocktail {
+	cocktail := common.Cocktail{}
+	pool.QueryRow(context.Background(), "select name, description, vol from cocktails where name = $1;", cocktailName).
+	Scan(&cocktail.Name, &cocktail.Description, &cocktail.Vol)
+
+	return cocktail
+}
+
+func getQueryString(availableIngredients []string) string {
+	arrLenght := len(availableIngredients)
+	queryStr := ""
+	for i, ingredient := range availableIngredients {
+		queryStr += "'" + ingredient + "'"
+		if i < arrLenght-1 {
+			queryStr += ", "
+		}
+	}
+	return "(" + queryStr + ")"
+}
+
+func computeCraftableCocktails(availableIngredients []string) []*common.Cocktail {
+	queryStr := getQueryString(availableIngredients)
+
+	cocktailIngredientMap := map[string][]common.Ingredient{}
+	cocktailIngredientCountMap := map[string]int{}
+	rows, err := pool.Query(
+		context.Background(),
+		"select c.name, c.ingredient_count, i.name, i.description, i.vol from cocktail_ingredients INNER JOIN ingredients i ON i.ingredient_id = cocktail_ingredients.ingredient_id INNER JOIN cocktails c ON c.cocktail_id = cocktail_ingredients.cocktail_id WHERE i.name IN " + queryStr,
+	)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to get craftableCocktail data %v\n", err)
+		// os.Exit(1)
+	}
+
+	for rows.Next() {
+		cocktailName := ""
+		ingredientCount := 0
+		ingredientName := ""
+		ingredientDescription := ""
+		ingredientVol := 0
+		rows.Scan(&cocktailName, &ingredientCount, &ingredientName, &ingredientDescription, &ingredientVol)
+		cocktailIngredientCountMap[cocktailName] = ingredientCount
+		cocktailIngredientMap[cocktailName] = append(
+			cocktailIngredientMap[cocktailName],
+			common.Ingredient{Name: ingredientName, Description: ingredientDescription, Vol: ingredientVol},
+		)
+	}
+
+	craftableCocktailArr := []*common.Cocktail{}
+	for key, ingredientArr := range cocktailIngredientMap {
+		ingredientCount := cocktailIngredientCountMap[key]
+		// fmt.Println("key: ", key, " ingredientCount: ", ingredientCount, " ingredientArr: ", ingredientArr)
+		if len(ingredientArr) == ingredientCount {
+			fmt.Println(key + "を作れるよ！")
+			cocktail := queryCocktail(key)
+			cocktail.Ingredients = ingredientArr
+			craftableCocktailArr = append(craftableCocktailArr, &cocktail)
+		}
+	}
+
+	for _, cocktail := range craftableCocktailArr {
+		fmt.Println(cocktail)
+	}
+
+	/*
+	select c.name, c.vol, c.ingredient_count, i.name from cocktail_ingredients
+	INNER JOIN ingredients i ON i.ingredient_id = cocktail_ingredients.ingredient_id
+	INNER JOIN cocktails c ON c.cocktail_id = cocktail_ingredients.cocktail_id
+	WHERE i.name IN ('ジン', 'トニックウォーター')
+	上記のクエリで現在持っている材料が使われているカクテルを取得できる
+	そして、カクテル名をキーに持つmapを作成してvalに材料をほりこむ
+	材料の配列の長さがカクテルの材料数と一致したら、そのカクテルは作れる
+	key: カクテル名 val []材料名
+	key: カクテル名 val 材料数
+	*/
+
+	return craftableCocktailArr
+}
+
 func responseAllIngredients(ctx *gin.Context) {
 	ingredients := getAllIngredients()
 	ctx.JSON(200, ingredients)
 }
 
 func responseCocktails(ctx *gin.Context) {
-	cocktails := setup.CocktailArr
+	query := ctx.Request.URL.Query()
+	availableIngredients := query["ingredients[]"]
+	fmt.Println("availableIngredients: ", availableIngredients)
+	fmt.Printf("type: %T\n ", availableIngredients)
+
+	cocktails := computeCraftableCocktails(availableIngredients)
+
 	ctx.JSON(200, cocktails)
 }
