7c1e08c4eda620ec70fba540b6aeafd788295ef7
SEKI-YUTA <zard.since1991@gmail,com>
2023-08-25 14:39:33 +0900

    酒とカクテルをいくつか追加

--- setUp/data.go
+++ setUp/data.go
@@ -36,17 +36,23 @@ const dbURL string = "postgres://root:root@localhost:5432/cocktail_db"
 const cocktail_table string = "cocktails"
 const ingredient_table string = "ingredients"
 
+var Absinthe = IngredientFactory("アブサン", "", 70)
 var Gin = IngredientFactory("ジン", "", 40)
 var Vodka = IngredientFactory("ウォッカ", "",  40)
 var Ram = IngredientFactory("ラム", "", 40)
 var Whiskey = IngredientFactory("ウィスキー", "", 40)
-var Cassis = IngredientFactory("カシス", "", 40)
-var fuzzyNavel = IngredientFactory("ファジーネーブル", "", 40)
-// plumWine := IngredientFactory("梅酒", "", true, 40)
-// brandy := IngredientFactory("ブランデー", "", true, 40)
+var Curacao = IngredientFactory("キュラソー", "", 40)
+var Tequila = IngredientFactory("テキーラ", "", 40)
+var Vermouth = IngredientFactory("ベルモット", "", 15)
+var Cassis = IngredientFactory("カシス", "", 15)
+var fuzzyNavel = IngredientFactory("ファジーネーブル", "", 15)
+var PlumWine = IngredientFactory("梅酒", "", 40)
+var Brandy = IngredientFactory("ブランデー", "", 40)
+var Beer = IngredientFactory("ビール", "", 0)
 var GigerAle = IngredientFactory("ジンジャーエール", "ジンジャーの味がする炭酸飲料", 0)
 var OrangeJuice = IngredientFactory("オレンジジュース", "", 0)
-// limeJuice := IngredientFactory("ライムジュース", "ジンジャーの味がする炭酸飲料", false, 0)
+var LimeJuice = IngredientFactory("ライムジュース", "", 0)
+var TonicWater = IngredientFactory("トニックウォーター", "", 0)
 
 var CocktailArr = []*common.Cocktail{
 	CocktailFactory("ジンバッグ", "ジンベースのカクテル", 15, 2, []common.Ingredient{
@@ -66,5 +72,14 @@ var CocktailArr = []*common.Cocktail{
 	}),
 	CocktailFactory("ピーチオレンジ", "", 15, 2, []common.Ingredient{
 		*fuzzyNavel, *OrangeJuice,
+	}),
+	CocktailFactory("モスコミュール", "", 15, 2, []common.Ingredient{
+		*Vodka, *GigerAle, *LimeJuice,
+	}),
+	CocktailFactory("梅酒ビア", "", 15, 2, []common.Ingredient{
+		*Beer, *PlumWine,
+	}),
+	CocktailFactory("ブランデートニック", "", 15, 2, []common.Ingredient{
+		*Brandy, *TonicWater,
 	}),
 }
