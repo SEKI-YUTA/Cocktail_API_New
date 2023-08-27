c15a3023abcedbcc4f07619edfc2f08859b5e6d7
SEKI-YUA <zard.since1991@gmail.com>
2023-08-27 23:44:09 +0900

    APIサーバーのプログラムをテーブルの構造変更に対応 ただしプログラムが汚くなったので一旦整理する必要がありそう

--- common/model.go
+++ common/model.go
@@ -1,19 +1,24 @@
 package common
 
 type Ingredient struct {
+    IngredientId int
     ShortName string
     LongName string
-    Category string
     Description string
     Vol int
+    IngredientCategoryId int
+    Category string
 }
 
 type Cocktail struct {
+    CocktailId int
     Name string
-    Category string
     Description string
     Vol int
     ParentName string
+    ParentCocktailId int
 	IngredientCount int
+    Category string
+    CocktailCategoryId int
     Ingredients []Ingredient
 }
--- server/server.go
+++ server/server.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
@@ -15,51 +15,55 @@ var pool *pgxpool.Pool
 
 
 func StartServer() {
-	// fmt.Println("server.go start")
+	fmt.Println("server.go start")
 
-	// // fmt.Println("user name: " + first_user)
-	// connConfig, err := pgx.ParseConfig(DB_URL)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "failed to parse db config")
-	// 	os.Exit(1)
-	// }
+	// fmt.Println("user name: " + first_user)
+	connConfig, err := pgx.ParseConfig(DB_URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to parse db config")
+		os.Exit(1)
+	}
 
-	// poolConfig, err := pgxpool.ParseConfig("")
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "failed to parse pool config")
-	// 	os.Exit(1)
-	// }
-	// poolConfig.ConnConfig = connConfig
+	poolConfig, err := pgxpool.ParseConfig("")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to parse pool config")
+		os.Exit(1)
+	}
+	poolConfig.ConnConfig = connConfig
 
-	// pool, err = pgxpool.ConnectConfig(context.Background(), poolConfig)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "failed to connect to db")
-	// 	os.Exit(1)
-	// }
+	pool, err = pgxpool.ConnectConfig(context.Background(), poolConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to connect to db")
+		os.Exit(1)
+	}
 
-	// defer pool.Close()
+	defer pool.Close()
 
-	// fmt.Println("start app")
-	// router := gin.Default()
-	// router.GET("/ingredients", responseAllIngredients)
-	// router.GET("/cocktails", responseCocktails)
-	// router.Run("localhost:9090")
-	// fmt.Println("end app")
+	fmt.Println("start app")
+	router := gin.Default()
+	router.GET("/ingredients", responseAllIngredients)
+	router.GET("/cocktails", responseCocktails)
+	router.Run("localhost:9090")
+	fmt.Println("end app")
 }
 
 func getAllIngredients() []common.Ingredient {
-	rows, err := pool.Query(context.Background(),"select name, description, vol from ingredients;")
+	/*
+	select * from ingredients 
+	INNER JOIN ingredient_categories ic ON  ingredients.ingredient_category_id=ic.ingredient_category_id
+	*/
+	rows, err := pool.Query(context.Background(),
+	"select ingredient_id, shortname, longname, description, vol, ic.ingredient_category_id, ic.name from ingredients INNER JOIN ingredient_categories ic ON  ingredients.ingredient_category_id=ic.ingredient_category_id;")
 	// rows, err := conn.Query(context.Background(),"select * from user_list;")
 	// err = conn.QueryRow(context.Background(), "select name from user_list where id = 1;").Scan(&first_user)
 	// defer rows.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to get user name %v\n", err)
 		os.Exit(1)
 	}
 	ingredients := []common.Ingredient{}
 	for rows.Next() {
 		var i common.Ingredient
-		err := rows.Scan(&i.ShortName, &i.Description, &i.Vol)
+		err := rows.Scan(&i.IngredientId,&i.ShortName, &i.LongName, &i.Description, &i.Vol, &i.IngredientCategoryId, &i.Category)
 		if err != nil {
 			fmt.Println("failed to scan data")
 		}
@@ -68,10 +72,35 @@ func getAllIngredients() []common.Ingredient {
 	return ingredients
 }
 
+func queryCocktailById(id int) common.Cocktail {
+	cocktail := common.Cocktail{}
+	pool.QueryRow(
+		context.Background(),
+		"SELECT cocktail_id, cocktails.name, description, cc.cocktail_category_id, cocktails.vol, ingredient_count, parent_cocktail_id from cocktails " +
+		"INNER JOIN cocktail_categories cc ON cocktails.cocktail_category_id=cc.cocktail_category_id WHERE cocktails.cocktail_id=$1",
+		id,
+		).Scan(&cocktail.CocktailId, &cocktail.Name, &cocktail.Description, &cocktail.CocktailCategoryId, &cocktail.Vol, &cocktail.IngredientCount, &cocktail.ParentCocktailId)
+		fmt.Println("parentCocktail name ", cocktail.Name)
+	return cocktail
+}
+
+
 func queryCocktail(cocktailName string) common.Cocktail {
 	cocktail := common.Cocktail{}
-	pool.QueryRow(context.Background(), "select name, description, vol from cocktails where name = $1;", cocktailName).
-	Scan(&cocktail.Name, &cocktail.Description, &cocktail.Vol)
+	fmt.Println("cocktailName: ", cocktailName)
+	pool.QueryRow(
+		context.Background(),
+		"SELECT cocktail_id, cocktails.name, description, cc.cocktail_category_id, cc.name, cocktails.vol, ingredient_count, parent_cocktail_id from cocktails " +
+		"INNER JOIN cocktail_categories cc ON cocktails.cocktail_category_id=cc.cocktail_category_id " +
+		"WHERE cocktails.name=" + "'" + cocktailName + "';",
+		// "select cocktails.name, description, vol from cocktails where name = $1;",
+		// ).Scan(&cocktail.Name, &cocktail.Description, &cocktail.Vol)
+		).Scan(&cocktail.CocktailId, &cocktail.Name, &cocktail.Description, &cocktail.CocktailCategoryId, &cocktail.Category, &cocktail.Vol, &cocktail.IngredientCount, &cocktail.ParentCocktailId)
+
+	if(cocktail.ParentCocktailId != 0) {
+		fmt.Println("parentCocktailId: ", cocktail.ParentCocktailId)
+		cocktail.ParentName = queryCocktailById(cocktail.ParentCocktailId).Name
+	}
 
 	return cocktail
 }
@@ -90,12 +119,17 @@ func getQueryString(availableIngredients []string) string {
 
 func computeCraftableCocktails(availableIngredients []string) []*common.Cocktail {
 	queryStr := getQueryString(availableIngredients)
+	fmt.Println("queryStr: ", queryStr)
 
 	cocktailIngredientMap := map[string][]common.Ingredient{}
 	cocktailIngredientCountMap := map[string]int{}
 	rows, err := pool.Query(
 		context.Background(),
-		"select c.name, c.ingredient_count, i.name, i.description, i.vol from cocktail_ingredients INNER JOIN ingredients i ON i.ingredient_id = cocktail_ingredients.ingredient_id INNER JOIN cocktails c ON c.cocktail_id = cocktail_ingredients.cocktail_id WHERE i.name IN " + queryStr,
+		"select c.name, c.ingredient_count, ic.name, i.ingredient_id, i.shortname, i.longname, i.description, i.vol, i.ingredient_category_id from cocktail_ingredients " +
+		"INNER JOIN ingredients i ON i.ingredient_id = cocktail_ingredients.ingredient_id " +
+		"INNER JOIN cocktails c ON c.cocktail_id = cocktail_ingredients.cocktail_id " +
+		"INNER JOIN ingredient_categories ic ON i.ingredient_category_id = ic.ingredient_category_id " +
+		"WHERE i.longname IN " + queryStr,
 	)
 
 	if err != nil {
@@ -104,16 +138,21 @@ func computeCraftableCocktails(availableIngredients []string) []*common.Cocktail
 	}
 
 	for rows.Next() {
+		fmt.Println("rows.Next()")
 		cocktailName := ""
 		ingredientCount := 0
-		ingredientName := ""
+		ingredientCategory := ""
+		ingredientId := 0
+		ingredientShortName := ""
+		ingredientLongName := ""
 		ingredientDescription := ""
 		ingredientVol := 0
-		rows.Scan(&cocktailName, &ingredientCount, &ingredientName, &ingredientDescription, &ingredientVol)
+		ingredientCategoryId := 0
+		rows.Scan(&cocktailName, &ingredientCount, &ingredientCategory, &ingredientId, &ingredientShortName, &ingredientLongName, &ingredientDescription, &ingredientVol, &ingredientCategoryId)
 		cocktailIngredientCountMap[cocktailName] = ingredientCount
 		cocktailIngredientMap[cocktailName] = append(
 			cocktailIngredientMap[cocktailName],
-			common.Ingredient{ShortName: ingredientName, Description: ingredientDescription, Vol: ingredientVol},
+			common.Ingredient{IngredientId: ingredientId,Category: ingredientCategory, ShortName: ingredientShortName, LongName: ingredientLongName, Description: ingredientDescription, Vol: ingredientVol, IngredientCategoryId: ingredientCategoryId},
 		)
 	}
 
