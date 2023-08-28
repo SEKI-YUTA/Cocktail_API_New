74438a97d83cb0f25e39f366aa37c2bd60160088
SEKI-YUA <zard.since1991@gmail.com>
2023-08-28 10:26:26 +0900

    jsonのレスポンスが小文字のフィールド名になるように変更

--- common/model.go
+++ common/model.go
@@ -1,24 +1,24 @@
 package common
 
 type Ingredient struct {
-    IngredientId int
-    ShortName string
-    LongName string
-    Description string
-    Vol int
-    IngredientCategoryId int
-    Category string
+    IngredientId int `json:"ingredient_id"`
+    ShortName string `json:"short_name"`
+    LongName string `json:"long_name"`
+    Description string `json:"description"`
+    Vol int `json:"vol"`
+    IngredientCategoryId int `json:"ingredient_category_id"`
+    Category string `json:"category"`
 }
 
 type Cocktail struct {
-    CocktailId int
-    Name string
-    Description string
-    Vol int
-    ParentName string
-    ParentCocktailId int
-	IngredientCount int
-    Category string
-    CocktailCategoryId int
-    Ingredients []Ingredient
+    CocktailId int `json:"cocktail_id"`
+    Name string `json:"name"`
+    Description string `json:"description"`
+    Vol int `json:"vol"`
+    ParentName string `json:"parent_name"`
+    ParentCocktailId int `json:"parent_cocktail_id"`
+	IngredientCount int `json:"ingredient_count"`
+    Category string `json:"category"`
+    CocktailCategoryId int `json:"cocktail_category_id"`
+    Ingredients []Ingredient `json:"ingredients"`
 }
